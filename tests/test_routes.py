001f3b529f3e9bc4fb57dbcae1a819e50e139d0e
allenyonemoto <167946824+allenyonemoto@users.noreply.github.com>
2024-06-21 20:50:57 +0900

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,4 +1,4 @@
-"""
+ """
 Account API Service Test Suite
 
 Test cases can be run with the following:
@@ -21,6 +21,7 @@ DATABASE_URI = os.getenv(
 BASE_URL = "/accounts"
 HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 
+
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
@@ -35,6 +36,7 @@ class TestAccountService(TestCase):
         app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
         app.logger.setLevel(logging.CRITICAL)
         init_db(app)
+        talisman.force_https = False
 
     @classmethod
     def tearDownClass(cls):
@@ -125,6 +127,7 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
     # ADD YOUR TEST CASES HERE ...
+
     def test_get_account(self):
         """It should Read a single Account"""
         account = self._create_accounts(1)[0]
